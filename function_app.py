36b4d5e25ae30e368d1c239c75a298795ad28979
Tom Boone <tbc@tomboone.com>
2025-09-08 16:00:57 -0400

    add github actions

--- function_app.py
+++ function_app.py
@@ -1,6 +1,8 @@
+"""Azure Function App"""
 import azure.functions as func
-import datetime
-import json
-import logging
+
+from alma_item_checks_update_service.blueprints.bp_update import bp as bp_update
 
 app = func.FunctionApp()
+
+app.register_blueprint(bp_update)
