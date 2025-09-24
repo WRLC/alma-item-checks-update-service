8e6fbcba80f0109f6db052d93a5ee07f4ce2c399
Tom Boone <tbc@tomboone.com>
2025-09-24 14:51:40 -0400

    add python version terraform output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "function_app_resource_group_name" {
   description = "The resource group of the function app."
   value       = azurerm_linux_function_app.function_app.resource_group_name
 }
+
+output "stage_slot_name" {
+  description = "The name of the app's staging deployment slot"
+  value       = azurerm_linux_function_app_slot.staging_slot.name
+}
