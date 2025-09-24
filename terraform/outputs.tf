740e7657e2f55a110ff46697d7219c9d80d87d7e
Tom Boone <tbc@tomboone.com>
2025-09-24 15:03:09 -0400

    add new actions and updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,11 @@ output "function_app_resource_group_name" {
   value       = azurerm_linux_function_app.function_app.resource_group_name
 }
 
+output "python_version" {
+  description = "The Python version used for the Function App"
+  value       = azurerm_linux_function_app.function_app.site_config[0].application_stack[0].python_version
+}
+
 output "stage_slot_name" {
   description = "The name of the app's staging deployment slot"
   value       = azurerm_linux_function_app_slot.staging_slot.name
